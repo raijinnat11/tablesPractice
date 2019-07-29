6995ed8a8a8bee8ac0cb9b456bbdc9145750f009
Raitah Jinnat <jinnatr@bxscience.edu>
2019-07-29 11:47:28 -0400

    Completed Iteration 2

--- tablesPractice/Class:BiasesTableTableViewController.swift
+++ tablesPractice/Class:BiasesTableTableViewController.swift
@@ -8,27 +8,46 @@
 
 import UIKit
 
-class Class_BiasesTableTableViewController: UITableViewController {
+struct Biases {
+    var id : Int
+    var title : String
+    var text : String
+    var image : String
+}
 
+class Class_BiasesTableTableViewController: UITableViewController {
+var allBiases = [
+    Biases(id: 1,
+           title: "Chungha",
+        text: "Currently a solo artist, she was a former member of the project girl group I.O.I",
+        image: "chungha"),
+    Biases(id: 2,
+           title: "Rosé",
+           text: "She is the main vocalist of the girl group Blackpink, from YG Entertainment.",
+           image: "rose"),
+    Biases(id: 3,
+           title: "Soyeon",
+           text: "She is the main rapper and leader of the group (G)-Idle from Cube Entertainment. She composes many of the group's songs and writes their lyrics as well",
+           image: "soyeon")
+    ]
     
 
-    // MARK: - Table view data source
+   
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 3
+       
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 5
+        return allBiases.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
 
-         cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
+         cell.textLabel?.text = allBiases[indexPath.row].title
 
         return cell
     }
@@ -39,50 +58,5 @@ class Class_BiasesTableTableViewController: UITableViewController {
         
     }
  
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
